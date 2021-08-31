8338adaa8c7e028460e5256cf9ff824bc4c7ae24
mustafa130501 <mustafa130501@gmail.com>
2021-08-31 21:46:00 +0300

    hw 1 commit

---
+++ javascript-dersleri/hw1.js
@@ -0,0 +1,6 @@
+var readline = require('readline');
+var RL = readline.createInterface(process.stdin, process.stdout);
+RL.question('Enter input? ',(test)=>{
+    console.log('Hello, World!');
+    console.log(test);
+});
