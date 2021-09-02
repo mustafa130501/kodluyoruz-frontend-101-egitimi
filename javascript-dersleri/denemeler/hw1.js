89da241debc1aab114c8c1da28cd6f2ca7196837
mustafa130501 <mustafa130501@gmail.com>
2021-09-02 21:11:03 +0300

    jshw1

--- javascript-dersleri/denemeler/hw1.js
+++ javascript-dersleri/denemeler/hw1.js
@@ -3,4 +3,9 @@ var RL = readline.createInterface(process.stdin, process.stdout);
 RL.question('Enter input? ',(test)=>{
     console.log('Hello, World!');
     console.log(test);
+
+   
+    
 });
+
+
---
+++ javascript-dersleri/denemeler/test.js
@@ -0,0 +1,10 @@
+let okulNumarasi =  414;
+if(true){
+ let okulNumarasi =  245;
+ console.log("Ilk blockta ki deger: "+okulNumarasi);
+  if(true){
+     let okulNumarasi = 312;
+    console.log("Ikinci blockta ki deger: "+okulNumarasi);
+     }
+}
+ console.log("Global scopeta ki deger: "+okulNumarasi);
